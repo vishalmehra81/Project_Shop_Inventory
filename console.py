c034cf3a8e5ce169dafc892027b7dfa7ed3fe14d
Vishal Mehra <67109899+vishalmehra81@users.noreply.github.com>
2020-08-09 09:42:03 +0100

    Defined console.py

---
+++ console.py
@@ -0,0 +1,24 @@
+import pdb
+from models.brand import Brand
+from models.product import Product
+from models.stock import Stock
+
+import repositories.brand_repository as brand_repository
+import repositories.product_repository as product_repository
+import repositories.stock_repository as stock_repository
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pdb.set_trace()
