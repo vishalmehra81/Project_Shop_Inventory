f43086881624fa3aab9537c6d9beed0597ff256d
Vishal Mehra <67109899+vishalmehra81@users.noreply.github.com>
2020-08-09 19:59:47 +0100

    CRUD stock_repository

--- repositories/stock_repository.py
+++ repositories/stock_repository.py
@@ -1,6 +1,36 @@
-# save
-# select_all
-# select
-# delete
-# delete_all
-# update
+from db.run_sql import run_sql
+from models.stock import Stock
+
+def save(stock):
+    sql = "INSERT INTO stocks (qty) VALUES (%s) RETURNING id "
+    values = [stock.qty]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    stock.id = id
+
+def select(all):
+    stocks = []
+    sql = "SELECT * FROM stocks"
+    results = run_sql[sql, values]
+    for result in results:
+        stock = Stock(result["qty"], result["id"])
+        stocks.append(stock)
+        return stocks
+
+def select(id):
+    sql = "SELECT * FROM stocks WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    stock = Stock(result["qty"], result["id"])
+    return stock
+
+def delete_all():
+    sql = "DELETE FROM stocks"
+    run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM stocks WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
