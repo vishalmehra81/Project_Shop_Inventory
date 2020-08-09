9a616d0274b309fc129aab0c000f1a197f59f3b2
Vishal Mehra <67109899+vishalmehra81@users.noreply.github.com>
2020-08-09 19:39:45 +0100

    CRUD product_repository

--- repositories/product_repository.py
+++ repositories/product_repository.py
@@ -14,14 +14,35 @@ def save(product):
 
 def select_all():
     products = []
-    
+    sql = "SELECT * FROM products"
+    results = run_sql(sql)
+    for result in results:
+        brand = brand_repository.select[result["brand_id"]]
+        stock = stock_repository.select[result["stock_id"]]
+        product = Product(result["name"]),brand, stock, result["category"],result["size"], result["cost_price"], result["selling_price"]
+        products.append(product)
+        return products
+
+def select(id):
+    sql = "SELECT * FROM products WHERE id = %s"
+    values = [id]
+    result = run_sql(sql,values)[0]
+    brand = brand_repository.select[result["brand_id"]]
+    stock = stock_repository.select[result["stock-id"]]
+    product = Product(result["name"], brand, stock, result["category"]), result["size"], result["cost_price"], result["selling_price"]
+    return product
+
+def delete_all():
+    sql = "DELETE FROM products"
+    run_sql(sql)
 
 
+def delete(id):
+    sql = "DELETE FROM products WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
 
-
-
-
-# select
-# delete
-# delete_all
-# update
+def update(product):
+    sql = "UPDATE products SET (name, brand_id, stock_id, category, size, cost_price, selling_price) VALUES (%s,%s,%s,%s,%s,%s,%s) WHERE id = %s"
+    values = [product.name, brand.brand.id, stock.stock.id, product.category, product.size, product.cost_price, product.selling_price]
+    run_sql(sql, values)
