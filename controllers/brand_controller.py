7d3fd6f8062c7837e89d4d6d5c59d04226bb7ec8
Vishal Mehra <67109899+vishalmehra81@users.noreply.github.com>
2020-08-10 01:19:16 +0100

    Updated index - brand controller

--- controllers/brand_controller.py
+++ controllers/brand_controller.py
@@ -4,3 +4,9 @@ from models.brand import Brand
 import repositories.brand_repository as brand_repository
 
 brands_blueprint = Blueprint("brands", __name__)
+
+# INDEX
+@brands_blueprint.route("/brands")
+def brands():
+    brands = brand_repository.select_all()
+    return render_template("brands/index.html", brands=brands)
