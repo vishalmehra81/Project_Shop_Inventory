b355ce24895ccf82029c701098603389d9d6ced7
Vishal Mehra <67109899+vishalmehra81@users.noreply.github.com>
2020-08-10 02:57:51 +0100

    defined index - stock_controller

--- controllers/stock_controller.py
+++ controllers/stock_controller.py
@@ -4,3 +4,9 @@ from models.stock import Stock
 import repositories.stock_repository as stock_repository
 
 stocks_blueprint = Blueprint("stocks", __name__)
+
+# INDEX
+@stocks_blueprint.route("/stocks")
+def stocks():
+    stocks = stock_repository.select_all()
+    return render_template("stocks/index.html", stocks=stocks)
